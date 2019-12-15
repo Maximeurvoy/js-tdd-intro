062c1f7914536c243d14c9ab38c3801c9614789a
Maximeurvoy <maxime.ptitx@gmail.com>
2019-12-15 16:06:13 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -2,21 +2,14 @@ const assert = require ('assert');
 
 
 const capitalizeFirstLetter=(string)=>{
-if (string.length != 0){
-  const words = string.split(' ')
-const test =  words.map(word=>word[0].toUpperCase()+ word.slice(1))
-
-return test.join(' ')
-}else{
-return ('')
+  if (string.length != 0){
+    const words = string.split(' ')
+    const test =  words.map(word=>word[0].toUpperCase()+ word.slice(1))
+    return test.join(' ')
+  }else{
+    return ('')
+  }
 }
-}
-
-console.log(capitalizeFirstLetter('test'))
-console.log(capitalizeFirstLetter('test truc'))
-console.log(capitalizeFirstLetter(''))
-
-
 
 assert.strictEqual(typeof capitalizeFirstLetter, 'function');
 assert.strictEqual(capitalizeFirstLetter('javaScript test'), 'JavaScript Test');
